ab6d8d01ec4bcd33dc53731fde7eaf141e205c79
TrineNyholm <TrineNyholmu@github.com>
2017-04-09 19:09:17 +0200

    T

---
+++ Python/figure_script/Ideal_low_plot.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 10 11:53:02 2017
+
+@author: Trine
+"""
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+w_range = np.linspace(0, np.pi, 1000)
+#n = np.linspace(0,10,len(w_range))
+
+
+# plot at ideal lavpas filter w_range(10000), og n(11)
+def h(n,w_c):
+    h = np.zeros(len(n))
+    for i in range(len(h)):
+        h[i]=(np.sin(n[i]*w_c))/((np.pi*n[i]))
+    return h 
+
+n = range(11)
+cutoff = np.pi/2 
+
+def H(w):
+    H = np.zeros(len(w))
+    for i in range(len(w)):
+        if np.abs(w[i])< np.pi/2 :
+            H[i] = 1
+        else :
+            H[i] = 0
+    return H
+
+
+plt.stem(n,h(n,cutoff))
+plt.xlabel("n", fontsize=15)
+plt.ylabel("h[n]", fontsize=15)
+plt.savefig("figures/ideal_low1.pdf")
+plt.show()
+
+plt.plot(w_range,H(w_range))
+plt.axis([0, np.pi, 0, 1.5])
+plt.xlabel("$\omega$", fontsize=20)
+plt.ylabel("|H($e^{j\omega}$)|", fontsize=15)
+plt.savefig("figures/ideal_low2.pdf")
+plt.show()
+
