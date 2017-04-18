fcdf02346ac2a4370d00c03b1deca0f850ace14b
TrineNyholm <TrineNyholmu@github.com>
2017-04-18 14:22:38 +0200

    T

--- Diskret-tids-systemer/Miniprojekt/Trine_filter_og_vindue.py
+++ Diskret-tids-systemer/Miniprojekt/Trine_filter_og_vindue.py
@@ -12,18 +12,21 @@ def impulse_response(n,M,omega1,omega2):
     hd = np.zeros(len(n))
     for i in range(len(n)):
         if n[i] == M/2.:
-            hd[i] = 1 - ((omega1 - omega2)/np.pi)
+            hd[i] = 1 - ((omega2 - omega1)/np.pi)
         else:
-            hd[i] = (1./(np.pi*(n[i]-(M/2.)))) * (np.sin(2*np.pi* omega2 * (n[i]-(M/2.))) - np.sin(2*np.pi* omega1 *(n[i]-(M/2.))))
+            hd[i] = (1./(np.pi*(n[i]-(M/2.)))) * (np.sin(omega2 * (n[i]-(M/2.))) - np.sin( omega1 *(n[i]-(M/2.))))
     return hd
 
     
-M = 20
-length = 100
+M = 40
+
+length = M
+samples = 100+length
 
 
-n = np.linspace(0,length,(length*8)+1)
-x = np.linspace(0, 2*np.pi, len(n))
+
+n = np.linspace(0,length,(samples)+1)
+x = np.linspace(0, length, len(n))
 
 omega1 =   5*np.pi/12. #(np.pi)/2. - delta 
 omega2 =   5*np.pi/8. # (np.pi)/2. + delta
@@ -33,7 +36,7 @@ hd = impulse_response(n,M,omega1,omega2)
 
 
 
-plt.plot(n,hd)
+plt.stem(n,hd)
 plt.axis([0,20,-1.5,1.5])
 plt.show()
 #==============================================================================
@@ -55,7 +58,7 @@ plt.show()
 
 h = w * hd
 plt.stem(n,h)
-plt.axis([-2,12,-0.8,0.8])
+plt.axis([-2,22,-0.8,0.8])
 plt.show()
 
 #==============================================================================
@@ -63,14 +66,14 @@ plt.show()
 #==============================================================================
 
 
-def fft(x,n):
+def fft(x):
     return np.fft.fft(x)
 
 def dB(X):
     return 20*np.log(np.abs(X))
 
-H = fft(h,n)
-W = fft(w,n)
+H = fft(h)
+W = fft(w)
 
 plt.plot(x,np.abs(W))
 plt.axis([0,1,-10,21])
@@ -81,4 +84,8 @@ plt.axis([0,1,-100,100])
 plt.show()
 
 plt.plot(x,H)
+
+plt.show()
+
+plt.plot(x,fft(hd))
 plt.show()
