887a6cf8af6684c48b1e023c41da5acc78025db6
Hilligsoe <cht15@student.aau.dk>
2017-05-01 19:11:04 +0200

    c

---
+++ Forsøg/impulsrespons.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 01 13:22:16 2017
+
+@author: cht15
+"""
+
+import numpy as np
+#==============================================================================
+# 
+#==============================================================================
+""" Impulsrespons Band Stop """
+def ImpulsresponsBS(n,M,cut1,cut2): # Den ønskværdige impulsrespons for et båndstop filter
+    hd = np.zeros(len(n))
+    for i in range(len(n)):
+        if n[i] == M/2:
+            hd[i] = 1 - (cut2 - cut1) * 2
+        else:
+            hd[i] = (np.sin(2 * np.pi * cut1 * (n[i] - M/2.)) / (np.pi*(n[i] - M/2.))) \
+            - (np.sin(2 * np.pi * cut2 * (n[i] - M/2.)) / (np.pi*(n[i] - M/2.)))
+    return hd
+
+""" Impulsrespons High Pass """
+def ImpulsresponsHP(n, M, f):     # Den ønskværdige impulsrespons for et højpass filter
+    hd = np.zeros(len(n))
+    for i in range(len(hd)):
+        if n[i] == M/2:
+            hd[i] = 1-f * 2
+        else:
+            hd[i] = (-np.sin(2 * np.pi * f * (n[i] - (M/2))))/(np.pi * (n[i] - (M/2)))
+    return hd       
+
+""" Impulserespons Low Pass """
+def ImpulsresponsLP(n, M, f):     # Den ønskværdige impulsrespons for et lavpass filter
+    hd = np.zeros(len(n))
+    for i in range(len(hd)):
+        if n[i] == M/2:
+            hd[i] = f * 2
+        else:
+            hd[i] = (np.sin(2 * np.pi * f*(n[i] - (M/2))))/(np.pi * (n[i] - (M/2)))
+    return hd 
+""" Impulserespons Band pass """
+def ImpulseresponseBP(n,M,ft1,ft2): # Den ønskværdige impulsrespons for et bandpass filter
+    h = np.zeros(len(n))
+    for i in range(len(n)):
+        if i == M/2:
+            h[i] = 2*(ft2 - ft1)
+        else:
+            h[i] = (1 / (np.pi*(i - M/2.)))*(np.sin(ft2*2*np.pi*(i - M/2.)) \
+            - (np.sin(ft1*2*np.pi*(i - M/2.))))
+    return h
+
+#==============================================================================
+# Noise adding
+#==============================================================================
+""" Noise adding """
+def add_noise(data,noise,c = 0.5): #kilde side 229 i DTSP, tager to signaler og addere de to 
+#    signal = np.convolve(data,noise)
+#    return signal
+    signal=np.zeros(len(data))    
+    for i in range(len(data)):
+        signal[i]=data[i]+float(c)*noise[i]
+    return signal
--- Forsøg/windowfunctions.py
+++ Forsøg/windowfunctions.py
@@ -9,6 +9,7 @@ import numpy as np
 #==============================================================================
 # Window function, for implementing filters or the STFT.
 #==============================================================================
+""" Rectangular vindue """
 def Rectangular(n,M):
     w = np.zeros(len(n))
     for i in range(len(n)):
@@ -18,15 +19,19 @@ def Rectangular(n,M):
             w[i] = 0
     return w
 
-#def Bartlett(n,M):
-#    w = np.zeros(len(n))
-#    for i in range(len(n)):
-#        if n[i] <= M/2:
-#            w[i]=(2*n[i])/M
-#        if n[i] > M/2 and n[i] <= M:
-#            w[i]=2-(2*n[i]/M)
-#    return w
-    
+""" BArtlett vindue """
+def Bartlett(n, M):                                         # Bartlett vindue
+    w = np.zeros(len(n))
+    for i in range(len(n)):
+        if n[i] <= M/2 and n[i] >= 0:
+            w[i] = ((2*n[i])/M)
+        elif n[i] > M/2 and n[i] <= M:
+            w[i] = 2-(2*n[i]/M)
+        else:
+            w[i] = 0
+    return w
+  
+""" Hanning vindue """    
 def Hanning(n,M):
     w = np.zeros(len(n))
     for i in range(len(n)):
@@ -35,7 +40,8 @@ def Hanning(n,M):
         else:
             w[i] = 0
     return w
- 
+
+""" Hamming vindue """ 
 def Hamming(n,M):
     w = np.zeros(len(n))
     for i in range(len(n)):
@@ -45,11 +51,23 @@ def Hamming(n,M):
             w[i] = 0
     return w
 
+""" Blackman vindue """
 def Blackman(n,M):
     w = np.zeros(len(n))
     for i in range(len(n)):
         if n[i] >= 0 and n[i] <= M:
             w[i]=0.42-0.5*np.cos((2*np.pi*n[i])/M)+0.08*np.cos(4*np.pi*n[i]/M)
+        else:
+            w[i] = 0
+    return w
+
+""" Kaiser vindue"""
+def Kaiser(n, M, B):                                        # Kaiservindue
+    w = np.zeros(len(n))
+    for i in range(len(n)):
+        if n[i] >= 0 and n[i] <= M:
+            variabel = B*np.sqrt(1-((n[i]-(M/2))/(M/2))**2)
+            w[i] = sc.special.i0(variabel)/sc.special.i0(B)
         else:
             w[i] = 0
     return w
