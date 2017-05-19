5fd196e18b49e1231f6cf211d929a4a2adbbd09b
mkda15 <martink.dalgaard@live.dk>
2017-05-19 21:54:55 +0200

    Rettelser

--- Python/figure_script/windows_plot.py
+++ Python/figure_script/windows_plot.py
@@ -20,61 +20,69 @@ from scipy import signal
 def w1(n,M):
     w = np.zeros(len(n))
     for i in range(len(n)):
-        if np.abs(n[i]) <= M:     
+        if np.abs(n[i]) <= M and n[i] >= 0:  
             w[i]=1
+        else:
+            w[i] = 0
     return w
 
 def w2(n,M):
     w = np.zeros(len(n))
     for i in range(len(n)):
-        if np.abs(n[i]) <= M/2:
+        if np.abs(n[i]) <= M/2 and n[i] >= 0:
             w[i]=(2*n[i])/M
-        if n[i] > M/2 and n[i] <= M:
+        elif n[i] > M/2 and n[i] <= M:
             w[i]=2-(2*n[i]/M)
+        else:
+            w[i] = 0
     return w
     
 def w3(n,M):
     w = np.zeros(len(n))
     for i in range(len(n)):
-        if n[i] <= M:
+        if n[i] <= M and n[i] >= 0:
             w[i]=0.5-0.5*np.cos((2*np.pi*n[i])/M)
+        else:
+            w[i] = 0
     return w
  
 def w4(n,M):
     w = np.zeros(len(n))
     for i in range(len(n)):
-        if n[i] <= M:
+        if n[i] <= M and n[i] >= 0:
             w[i]=0.54-0.46*np.cos((2*np.pi*n[i])/M)
+        else:
+            w[i] = 0
     return w
 
 def w5(n,M):
     w = np.zeros(len(n))
     for i in range(len(n)):
-        if n[i] <= M:
+        if n[i] <= M and n[i] >= 0:
             w[i]=0.42-0.5*np.cos((2*np.pi*n[i])/M)+0.08*np.cos(4*np.pi*n[i]/M)
+        else:
+            w[i] = 0
     return w
-M=1000
+
+M=100
 N=M+1
 
 sampels = 10000
-n = np.linspace(0,M,N) 
+n = np.linspace(-M/10,M+M/10,10*M)
 freq_ax = np.linspace(0,np.pi,sampels/2)
 w = w5(n,M)
 
-
-
-
-#plt.plot(n,w1(n,M), label = "Rectangular")
-#plt.plot(n,w2(n,M), label = "Bartlett")
-#plt.plot(n,w3(n,M), label = "Hanning")
-#plt.plot(n,w4(n,M), label = "Hamming")
-#plt.plot(n,w5(n,M), label = "Blackman")
-#plt.axis([0,M,0,1.2])
-#plt.legend(loc= "upper right", bbox_to_anchor=(1., 1))
-#plt.xlabel("n")
-#plt.ylabel("w [n]")
-##plt.savefig("figures/window_types.pdf")
-#plt.show()
+plt.plot(n,w1(n,M), "b-", label = "Rectangular")
+plt.plot(n,w2(n,M), "g-", label = "Bartlett")
+plt.plot(n,w3(n,M), "r-", label = "Hanning")
+plt.plot(n,w4(n,M), "c-", label = "Hamming")
+plt.plot(n,w5(n,M), "m-", label = "Blackman")
+plt.axis([-M/10,M+M/10,-0.2,1.2])
+plt.legend(loc= "best")
+plt.xlabel("$n$")
+plt.ylabel("$w[n]$")
+plt.savefig("figures/window_types.pdf")
+plt.show()
 
 W = np.abs(np.fft.fft(np.pad(w,(0,sampels-N),'constant',constant_values=0)))
 W_dB= 20 * np.log10(np.abs(W))
