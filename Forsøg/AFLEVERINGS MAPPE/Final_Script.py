4ae629c653c3833c341f830179d143483cb6c414
TrineNyholm <TrineNyholmu@github.com>
2017-05-22 13:08:38 +0200

    T

--- Forsøg/AFLEVERINGS MAPPE/Final_Script.py
+++ Forsøg/AFLEVERINGS MAPPE/Final_Script.py
@@ -175,30 +175,31 @@ plt.plot(tid,signal)  # Original data with noise added
 plt.xlabel('Time [s]')
 plt.ylabel('Amplitude')
 #plt.axis([0,6,-1,1])
-#plt.savefig("figures/integrationstest/signal.png")
+plt.savefig("figures/integrationstest/signal.png")
 plt.show()
 
 """ Filtered signal in time domain """
 plt.plot(tid,signal_filt)  # Original data with noise added 
 plt.xlabel('Time [s]')
 plt.ylabel('Amplitude')
-#plt.axis([0,6,-1,1])
-#plt.savefig("figures/integrationstest/f_signal.png")
+plt.axis([0,6,-6000,6000])
+plt.savefig("figures/integrationstest/f_signal.png")
 plt.show()
 
 """ Pure signal with noise i frekvency domain"""
 plt.plot(freq_axis[:plotlength],np.abs(SIGNAL)[:plotlength])          
 plt.xlabel('Frequency [Hz]')
 plt.ylabel('Amplitude')
-#plt.savefig("figures/integrationstest/FSIGNAL.png")
+#plt.savefig("figures/filter_test/f_SIGNAL.png")
+plt.savefig("figures/integrationstest/FSIGNAL.png")
 plt.show()
 
 """ Filtered signal in frequency domain """
 plt.plot(freq_axis[:plotlength],np.abs(SIGNAL_FILT[:plotlength]))   
 plt.xlabel('Frequency [Hz]')
 plt.ylabel('Amplitude')
-#plt.savefig("figures/integrationstest/f_FSIGNAL.png")
-#plt.savefig("figures/integrationstest/f_FSIGNAL.png")
+#plt.savefig("figures/filter_test/SIGNAL.png")
+plt.savefig("figures/integrationstest/f_FSIGNAL.png")
 plt.show()
 
 """ Close-up all data in time domain  """
@@ -217,28 +218,28 @@ print('plot plotteret 7/9')
 # Computing Spectrogram
 #==============================================================================
 
-#X,o,ws = stft(signal_filt)      # return STFT, stft and used window(time) 
-#
-#WS = np.abs(np.fft.fft(ws))                                 # window(frekvenscy) used in STFT  
-##X,v_w,v_t,t = stft_h(signal_filt,overlap = 2)
-#
-#print('stft udregnet 9/9') 
+X,o,ws = stft(signal_filt)      # return STFT, stft and used window(time) 
 
-#X = db(np.abs(X).T)                                         # Calculated to dB
-#
-#x = np.linspace(0,tid[-1],np.shape(X)[1])       
-#y = np.linspace(0,freq_axis[-1],np.shape(X)[0]) 
+WS = np.abs(np.fft.fft(ws))                                 # window(frekvenscy) used in STFT  
+#X,v_w,v_t,t = stft_h(signal_filt,overlap = 2)
 
-#spec = plt.pcolormesh(x,y[freq_inter1:freq_inter2],X[freq_inter1:freq_inter2],cmap='jet')
-#cb   = plt.colorbar(spec)
-#cb.set_label(label = 'Amplitude [dB]', fontsize=fontsize)
-#plt.xlabel('Time [s]', fontsize = fontsize)
-#plt.ylabel('Frequency [Hz]', fontsize = fontsize)
-#plt.axis([0,7.8,0,2000])
-##plt.savefig("figures/skala.png")
-##plt.savefig("figures/integrationstest/spectrogram.png")
-#plt.savefig("figures/systemtest/final_spec3.png")
-#plt.show()
+print('stft udregnet 9/9') 
+
+X = db(np.abs(X).T)                                         # Calculated to dB
+
+x = np.linspace(0,tid[-1],np.shape(X)[1])       
+y = np.linspace(0,freq_axis[-1],np.shape(X)[0]) 
+
+spec = plt.pcolormesh(x,y[freq_inter1:freq_inter2],X[freq_inter1:freq_inter2],cmap='jet')
+cb   = plt.colorbar(spec)
+cb.set_label(label = 'Amplitude [dB]', fontsize=fontsize)
+plt.xlabel('Time [s]', fontsize = fontsize)
+plt.ylabel('Frequency [Hz]', fontsize = fontsize)
+plt.axis([0,7.8,0,2000])
+#plt.savefig("figures/skala.png")
+#plt.savefig("figures/integrationstest/spectrogram.png")
+plt.savefig("figures/systemtest/final_spec3.png")
+plt.show()
 
 #==============================================================================
 # Peak Dectection
@@ -247,12 +248,12 @@ print('plot plotteret 7/9')
 max_freq_t = peak_dec(X,15,y)
 
 """ plot peak dection """
-#plt.plot(x,max_freq_t,'o')
-#plt.xlabel('Time [s]')
-#plt.ylabel('Frequency [Hz]')
-##plt.savefig("figures/integrationstest/peak_dec.png")
-#plt.savefig("figures/systemtest/final_peak3.png")
-#    
+plt.plot(x,max_freq_t,'o')
+plt.xlabel('Time [s]')
+plt.ylabel('Frequency [Hz]')
+#plt.savefig("figures/integrationstest/peak_dec.png")
+plt.savefig("figures/systemtest/final_peak3.png")
+    
 #==============================================================================
 # SNR
 #==============================================================================
